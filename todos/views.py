25677423f6110d4e36b2fedca8bceb98bdca938f
Anuj Khanal <anuz.khanal@gmail.com>
2022-02-10 00:44:41 +0200

    Update views.py

--- todos/views.py
+++ todos/views.py
@@ -1,13 +1,8 @@
-from rest_framework import status, response, permissions
-
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import authenticate
-
 
 from todos.models import Todo
 from .serializers import TodoSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
@@ -15,15 +10,17 @@ from django_filters.rest_framework import DjangoFilterBackend
 class TodosAPIView(ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
-    filter_backend = [DjangoFilterBackend]
-
-    filterset_fields = ["status"]
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
 
     def get_queryset(self):
-        return Todo.objects.filter(owner=self.request.user)
+        queryset = Todo.objects.filter(owner=self.request.user)
+        status_query = self.request.query_params.get("status", None)
+        if status_query is not None:
+            print("queryset", queryset)
+            queryset = queryset.filter(status=status_query)
+        return queryset
 
 
 class TodoDetailAPIView(RetrieveUpdateDestroyAPIView):
